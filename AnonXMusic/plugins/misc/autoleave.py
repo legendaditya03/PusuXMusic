83d26136718d9f6ef9d644acaf0a02bba596725e
legendaditya03 <147244657+legendaditya03@users.noreply.github.com>
2023-11-12 13:22:06 +0530

    Update autoleave.py

--- AnonXMusic/plugins/misc/autoleave.py
+++ AnonXMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1002112393618
-                                and i.chat.id != -1002112393618
+                                and i.chat.id != -1002136243377
+                                and i.chat.id != -1002136243377
                             ):
                                 if left == 20:
                                     continue
